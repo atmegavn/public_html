bba34b638148b7924eafebdd7903827ff032bb67
thanhlevan <thanhlevan0502@gmail.com>
2024-07-31 10:32:51 +0700

    add slider

--- js/main-script.js
+++ js/main-script.js
@@ -7,7 +7,7 @@ if (window.RS_MODULES === undefined) window.RS_MODULES = {};
 if (RS_MODULES.modules === undefined) RS_MODULES.modules = {};
 RS_MODULES.modules["revslider21"] = {
 	once: RS_MODULES.modules["revslider21"] !== undefined ? RS_MODULES.modules["revslider21"].once : undefined, init: function () {
-		window.revapi2 = window.revapi2 === undefined || window.revapi2 === null || window.revapi2.length === 0 ? document.getElementById("rev_slider_2_1") : window.revapi2;
+		window.revapi2 = window.revapi2 === undefined || window.revapi2 === null || window.revapi2.length === 0 ? document.getElementsByClassName("rev_slider_2_1") : window.revapi2;
 		if (window.revapi2 === null || window.revapi2 === undefined || window.revapi2.length == 0) { window.revapi2initTry = window.revapi2initTry === undefined ? 0 : window.revapi2initTry + 1; if (window.revapi2initTry < 20) requestAnimationFrame(function () { RS_MODULES.modules["revslider21"].init() }); return; }
 		window.revapi2 = jQuery(window.revapi2);
 		if (window.revapi2.revolution == undefined) { revslider_showDoubleJqueryError("rev_slider_2_1"); return; }
@@ -55,9 +55,9 @@ RS_MODULES.modules["revslider21"] = {
 				allowHTML5AutoPlayOnAndroid: true
 			},
 		});
-
 	}
 } // End of RevInitScript
+
 
 if (window.RS_MODULES.checkMinimal !== undefined) { window.RS_MODULES.checkMinimal(); };
 
